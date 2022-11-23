20c9c584d4f29f0eaf9da79d999f4ed319a57382
Miguel Angel <miguel@gmail.com>
2022-11-22 21:34:43 -0500

    Agregar archivos de proyecto.

---
+++ LocalDatabase/LocalDatabase/Views/AlbumesPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using LocalDatabase.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalDatabase.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlbumesPage : ContentPage
+    {
+        public AlbumesPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new AlbumesViewModel();
+        }
+    }
+}
